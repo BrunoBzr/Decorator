b7f7e4060e379a9aa61af66fe29ebc43d1f50b12
Bruno Pereira <brunopereirabzrdev@gmail.com>
2025-01-24 13:30:46 -0300

    Update MainArmas.java

--- Decorator/src/armas/MainArmas.java
+++ Decorator/src/armas/MainArmas.java
@@ -4,13 +4,23 @@ public class MainArmas {
     public static void main(String[] args) {
 
         //Dano só da arma
-        Bot bot = new Bot(200);
+        Bot bot = new Bot(100);
+        System.out.println("Vida do bot: " + bot.vida);
         Arma espada = new Espada();
         bot.tirarVida(espada.dano());
         System.out.println("Dano puro da espada: " + espada.dano() + ", Vida do BOT: " + bot.vida);
 
         //Espada com fogo
+        bot.vida = 100;
+        System.out.println("Vida do bot: " + bot.vida);
         espada = new Fogo(espada);
+        bot.tirarVida(espada.dano());
+        System.out.println("Dano puro da espada + dano de fogo: " + espada.dano() + ", Vida do BOT: " + bot.vida);
+
+
+        bot.vida = 100;
+        System.out.println("Vida do bot: " + bot.vida);
+        espada = new Veneno(espada);
         bot.tirarVida(espada.dano());
         System.out.println("Dano puro da espada + dano de fogo: " + espada.dano() + ", Vida do BOT: " + bot.vida);
 
